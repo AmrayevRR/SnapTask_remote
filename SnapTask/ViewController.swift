1f381d82e7b6d23270c0e1deb5bcd3f341b86b90
Ramir <amrayev.ramir@icloud.com>
2022-04-09 19:30:42 +0600

    Initial Commit

---
+++ SnapTask/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SnapTask
+//
+//  Created by Ramir Amrayev on 4/9/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
